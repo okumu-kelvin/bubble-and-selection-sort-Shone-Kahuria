9c0b3eeb7697689326dfad7212eeb5189cf21151
Shone-Kahuria <shonekahuria@gmail.com>
2025-06-20 09:40:36 +0300

    Refactor bubble sort and implement selection sort algorithms

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,11 +1,11 @@
 numbers = [5, 4, 3, 2, 1, 0]
 
 def bubble_sort(unsorted_list):
-    n= len(unsorted_list)
+    n = len(unsorted_list)
     for i in range(n):
-        for j in range(0, n-i-1):
-            if unsorted_list[j] > unsorted_list[j+1]:
-                unsorted_list[j], unsorted_list[j+1]= unsorted_list[j+1], unsorted_list[j]
-                return unsorted_list
-sorted_numbers= bubble_sort(numbers)
+        for j in range(0, n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+    return unsorted_list  
+sorted_numbers = bubble_sort(numbers)
 print(sorted_numbers)
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,14 @@
-def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+numbers= [5, 4, 3, 2, 1, 0]
+
+def selection_sort(unsorted_list):
+    n= len(unsorted_list)
+    for i in range(n):
+        min_idx= i
+        for j in range(i + 1, n):
+            if unsorted_list[j] < unsorted_list[min_idx]:
+                min_idx = j
+        unsorted_list[i], unsorted_list[min_idx] = unsorted_list[min_idx], unsorted_list[i]
+    return unsorted_list
+
+sorted_numbers = selection_sort(numbers)
+print(sorted_numbers)
